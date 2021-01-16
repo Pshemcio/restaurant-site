780f3ba2a78f8bc419786183dea5d431399592d0
Pshemcio <daglas92@gmail.com>
2021-01-16 16:44:42 +0100

    Add parallax effect to #pizza-break

--- js/main.js
+++ js/main.js
@@ -8,6 +8,7 @@ window.addEventListener('DOMContentLoaded', () => {
 const mainNav = document.getElementById('main-nav');
 const burgerBtn = mainNav.querySelector('.burger-btn');
 let initialNavHeight = getComputedStyle(document.documentElement).getPropertyValue('--logo-size');
+const parallaxBg = document.querySelector('.parallax-bg');
 
 const handleBurgerMenu = e => {
     if (!mainNav.classList.contains('menu-active') && !e.target.classList.contains('burger-btn')) {
@@ -21,21 +22,49 @@ const shrinkNavbar = () => {
     const parsedNavHeight = parseInt(initialNavHeight);
     if (window.innerWidth < 992) {
         return;
-    }
+    };
 
     if (window.scrollY > 50) {
         document.documentElement.style.setProperty('--logo-size', `${parsedNavHeight / 2}rem`);
     } else {
         document.documentElement.style.setProperty('--logo-size', initialNavHeight);
     };
-}
+};
 
 const checkResize = () => {
     initialNavHeight = getComputedStyle(document.documentElement).getPropertyValue('--logo-size');
-}
+};
+
+const getElementYOffset = (element) => {
+    const scrollOnWindow =
+        window.pageYOffset !== undefined
+            ? window.pageYOffset
+            : (document.documentElement || document.body.parentNode || document.body)
+                .scrollTop;
+    const rect = element.getBoundingClientRect();
+    let distanceFromTopOfPage = rect.top;
+    if (scrollOnWindow !== 0) {
+        distanceFromTopOfPage = rect.top + scrollOnWindow;
+    }
+
+    return distanceFromTopOfPage;
+};
+
+const parallaxEffect = (section) => {
+    const parallaxInitialScroll = section.getBoundingClientRect().top - window.outerHeight;
+    const parallaxFinishScroll = section.getBoundingClientRect().bottom;
+    const test = getElementYOffset(section) - window.outerHeight;
+
+    if (parallaxInitialScroll > 0 || parallaxFinishScroll < 0) {
+        return;
+    }
+
+    section.style.backgroundPositionY = `${100 - (window.scrollY - test) / 10}%`;
+};
 
 const scrollEvents = () => {
     shrinkNavbar();
+    parallaxEffect(parallaxBg);
 };
 
 // smooth scrolling for browsers that doesn't support it
